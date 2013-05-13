3d55a3fedfada5b5963b956036132670270e73ea
Clemens S. <gs12m015@technikum-wien.at>
2013-05-14 01:27:58 +0200

    Added #ifdefs for Mobile Touch Input + First Build for Android :)

--- Unity 3.5 Projects/Prototype/Assets/Scripts/Flight/ArrowControl.cs
+++ Unity 3.5 Projects/Prototype/Assets/Scripts/Flight/ArrowControl.cs
@@ -1,5 +1,10 @@
+#if UNITY_IPHONE || UNITY_ANDROID
+#	define MOBILE
+#endif
+
 using UnityEngine;
 using System.Collections;
+
 
 public class ArrowControl : MonoBehaviour {
 	
@@ -18,19 +23,35 @@ public class ArrowControl : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () 
-	{
+	{	
 		guiTexture.enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
+		bool touchDown;
+		bool touchUp;
 		
-		if(Input.GetMouseButtonDown(0))
+#if MOBILE
+		touchDown = Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began;
+		touchUp = Input.GetTouch(0).phase == TouchPhase.Ended;
+#else
+		touchDown = Input.GetMouseButtonDown(0);
+		touchUp = Input.GetMouseButtonUp(0);
+#endif
+		
+		if(touchDown) // Input.GetMouseButtonDown(0)
 		{
 			_mouseDown = true;
 			guiTexture.enabled = true;
+			
+#if MOBILE
+			Touch touch = Input.GetTouch(0);
+			_mouseDownPosition = new Vector3(touch.position.x, touch.position.y, 0);
+#else
 			_mouseDownPosition = Input.mousePosition;
+#endif
 			
 			float newPosX;
 			
@@ -51,7 +72,7 @@ public class ArrowControl : MonoBehaviour {
 			
 			_airCraft.EnableHUDControl(true);
 		}
-		else if(Input.GetMouseButtonUp(0))
+		else if(touchUp) // Input.GetMouseButtonUp(0)
 		{
 			_mouseDown = false;
 			guiTexture.enabled = false;
@@ -62,9 +83,18 @@ public class ArrowControl : MonoBehaviour {
 		// Mouse is currently hold down:
 		if(_mouseDown == true)
 		{
+			Vector3 curMousePos;
+			
+#if MOBILE
+			Touch touch = Input.GetTouch(0);
+			curMousePos = new Vector3(touch.position.x, touch.position.y, 0);
+#else
+			curMousePos = Input.mousePosition;
+#endif
+			
 			// Figure out, how far away the current mouse position is from the first touch down:
-			float diff = Input.mousePosition.x - _mouseDownPosition.x;
-			Debug.Log(diff);
+			float diff = curMousePos.x - _mouseDownPosition.x;
+//			Debug.Log(diff);
 			
 			diff *= 4;
 			
