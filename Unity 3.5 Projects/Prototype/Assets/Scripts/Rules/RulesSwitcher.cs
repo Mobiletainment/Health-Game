fc38ff091cd2d229f9bbef87da51db9b0c683926
David Pertiller <david.pertiller@gmail.com>
2013-05-15 04:23:59 +0200

    added RuleSwitcher and visualisation when rule is switched

--- Unity 3.5 Projects/Prototype/Assets/Scripts/Rules/RulesSwitcher.cs
+++ Unity 3.5 Projects/Prototype/Assets/Scripts/Rules/RulesSwitcher.cs
@@ -5,43 +5,26 @@ public class RulesSwitcher : MonoBehaviour
 {
 	public float ruleDuration = 5.0f; //#seconds before the next rule is applied
 	public float flashLength = 0.5f;
-	public Color flashColor;
 	
-	private GUITexture imageObject;
+	private Camera flashCamera;
 	
-	 //This is property syntax is js. It looks wierd, but it's a getter just like C#
 	void Start()
 	{
-		Texture2D tex = new Texture2D (1, 1);
-		tex.SetPixel(0, 0, Color.white);
-		tex.Apply();
-		
-		GameObject gameObj = new GameObject("Image Object");
-		gameObj.transform.position = new Vector3(0, 0, -45);
- 		gameObj.transform.localScale = new Vector3(0, 0, 1);
- 
-		
-		imageObject = gameObj.AddComponent<GUITexture>();
-		imageObject.texture = tex;
-		imageObject.color = flashColor;
-		imageObject.pixelInset = new Rect(0, 0, Screen.width, Screen.height);
-		imageObject.enabled = false;
-		Debug.Log(imageObject);
-		InvokeRepeating("FlashImage", 5.0f, ruleDuration);
+		flashCamera = GameObject.Find("Flash Camera").camera;
+		InvokeRepeating("FlashImage", 0.0f, ruleDuration);
 	}
 	
-
 	
 	protected void FlashImage ()
 	{
 		//Debug.Log("FlashImage");
-		imageObject.enabled = true;
+		flashCamera.enabled = true;
 		Invoke("HideImage", flashLength);
 	}
  
 	protected void HideImage ()
 	{
-		imageObject.enabled = false;
+		flashCamera.enabled = false;
 	} 
 	
 	
