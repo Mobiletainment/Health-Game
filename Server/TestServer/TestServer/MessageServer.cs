9ad385d6f5aee5d8053b590b66ea11df64b5ac04
juekowa <konrad.juergen@hotmail.com>
2013-10-30 00:55:43 +0100

    test server
    written a simple messsage server 4 teasting - working with
    browser-connections

---
+++ Server/TestServer/TestServer/MessageServer.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestServer
+{
+    public class MessageServer
+    {
+        public bool Run { set; get; }
+
+        private TcpListener _listener;
+        private Thread _listenThread;
+
+        private int _port = 37015;
+
+        int _threadCounter = 0;
+
+        public MessageServer()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            Run = true;
+            _listener = new TcpListener(IPAddress.Any, _port);
+            _listenThread = new Thread(new ThreadStart(ListenForClients));
+            _listenThread.Start();
+        }
+
+        private void ListenForClients()
+        {
+            _listener.Start();
+
+            while (Run)
+            {
+                TcpClient client = _listener.AcceptTcpClient();
+                Console.WriteLine("Client connected!");
+
+                Thread clientThread = new Thread(new ParameterizedThreadStart(ClientCommunication));
+                clientThread.Start(client);
+                ++_threadCounter;
+            }
+        }
+
+        private void ClientCommunication(object client)
+        {
+            using (TcpClient tcpClient = (TcpClient)client)
+            {
+                using (NetworkStream clientStream = tcpClient.GetStream())
+                {
+                    while (Run)
+                    {
+                        try
+                        {
+                            if (tcpClient.Available == 0)
+                                break;
+
+                            StreamReader sr = new StreamReader(clientStream);
+                            string getvar = sr.ReadLine();
+                            if (string.IsNullOrEmpty(getvar)) 
+                                break;
+
+                            Console.WriteLine("Msg received: " + getvar);
+
+                            while (!sr.EndOfStream)
+                            {
+                                string line = sr.ReadLine();
+                                Console.WriteLine(line);
+                                if (string.IsNullOrEmpty(line)) 
+                                    break;
+                            }
+
+                            StreamWriter sw = new StreamWriter(clientStream);
+                            sw.WriteLine("Message from server!");
+                            sw.Flush();
+
+                            Console.WriteLine("Sent message to Client.");
+
+                            sr.Close();
+                        }
+                        catch
+                        {
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
