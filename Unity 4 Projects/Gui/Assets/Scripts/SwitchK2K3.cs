02571f32e95c39267fbdb1112094b04c40fd5871
tnixeu <github@tnix.eu>
2013-11-07 06:09:10 +0100

    teil der gui fürs spiel... ich hoff es sind alle datein dabei die gebraucht werden.. hab aber ngui draussen gelassen.. weil das eh jeder selbst dazu haun kann (bin ma ned sicher ob das ne gute idee is.. aber woll auch ned extra 20mb eincheken...

---
+++ Unity 4 Projects/Gui/Assets/Scripts/SwitchK2K3.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchK2K3 : MonoBehaviour {
+
+	public GameObject currentPanelObject;
+    public GameObject nextPanelObject;
+		
+	public UIInput input;
+	public UILabel label;
+	public UIInput inputPW;
+
+    void OnClick() {
+        NGUITools.SetActive(nextPanelObject, true);
+        NGUITools.SetActive(currentPanelObject, false);
+		label.text="Hallo, "+NGUITools.StripSymbols(input.value)+",\n"+"hier hast du ein geheimes Team-Passowort:";
+		inputPW.value=Random.Range(10000000,99999999).ToString();
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Unity 4 Projects/Gui/Assets/Scripts/SwitchPanel.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchPanel : MonoBehaviour {
+	
+    public GameObject currentPanelObject;
+    public GameObject nextPanelObject;
+	
+    void OnClick() {
+        NGUITools.SetActive(nextPanelObject, true);
+        NGUITools.SetActive(currentPanelObject, false);
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
