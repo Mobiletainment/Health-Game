de4e0ce0428de023930204792e1cd7b86e243eba
David Pertiller <david.pertiller@gmail.com>
2013-12-03 22:32:34 +0100

    created analysis tools for psychologists for analyzing and evaluating feedback from parents

---
+++ Back-End/Server/public_html/analysis-constants.php
@@ -0,0 +1,8 @@
+<?php
+// database constants
+define("DB_SERVER", "localhost");
+define("DB_USER", "pertille_HealthG");
+define("DB_PASS", ",yX3KP+Il5wk");
+define("DB_NAME", "pertille_aquaSpace");
+define("DEBUG", false);
+?>
