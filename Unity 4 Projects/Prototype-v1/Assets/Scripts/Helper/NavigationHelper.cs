ac92db011c34f8cfff90b6c3f62a2672a33c3c94
David Pertiller <david.pertiller@gmail.com>
2013-12-08 21:12:38 +0100

    upgraded to Unity 4.3.1

---
+++ Unity 4 Projects/Prototype-v1/Assets/Scripts/Helper/NavigationHelper.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavigationHelper : MonoBehaviour {
+
+	public UserManager userManager;
+	public GameObject ParentMenu;
+
+	void Awake()
+	{
+		userManager = ScriptableObject.CreateInstance<UserManager> ();
+
+		userManager.ResetData(); //uncomment this to start from the beginning and not load the game directly on startup
+
+		if (userManager.IsLoggedIn())
+		{
+			Debug.Log("User is logged in");
+
+			if (userManager.IsChild == true)
+			{
+				LoadGameScene();
+			}
+			else
+				LoadParentMenu();
+		}
+	}
+
+	void Start()
+	{
+		LoadParentMenu();
+	}
+
+
+	public static void LoadGameScene()
+	{
+		Debug.Log("Loading Game Scene");
+		Screen.orientation = ScreenOrientation.LandscapeLeft;
+		Application.LoadLevel("TrackFlight");
+	}
+
+	public void LoadParentMenu()
+	{
+		MenuStack menuStack = MenuStack.Instance;
+		Debug.Log("MenuStack: " + menuStack);
+		menuStack._start = ParentMenu;
+	}
+}
