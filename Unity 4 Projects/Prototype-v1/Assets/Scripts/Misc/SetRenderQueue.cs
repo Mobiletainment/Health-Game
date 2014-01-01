3791f6d69bb2fabfef27bad3399bff9882d0546c
Clemens S <gs12m015@technikum-wien.at>
2014-01-02 00:11:40 +0100

    Could find a fix for the Rendering Problem. Keyword: Material.RenderQueue I hope this will not lead to other problems later on :P

---
+++ Unity 4 Projects/Prototype-v1/Assets/Scripts/Misc/SetRenderQueue.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Original Source: http://wiki.unity3d.com/index.php?title=SetRenderQueue
+
+[AddComponentMenu("Effects/SetRenderQueue")]
+[RequireComponent(typeof(Renderer))]
+public class SetRenderQueue : MonoBehaviour 
+{
+	public int queue = 3000;
+	
+	protected void Awake() 
+	{
+		if (!renderer || !renderer.sharedMaterial)
+			return;
+
+		renderer.sharedMaterial.renderQueue = queue;
+	}
+}
+
+// Original Code:
+//public class SetRenderQueue : MonoBehaviour {
+//	public int queue = 1;
+//	
+//	public int[] queues;
+//	
+//	protected void Start() {
+//		if (!renderer || !renderer.sharedMaterial || queues == null)
+//			return;
+//		renderer.sharedMaterial.renderQueue = queue;
+//		for (int i = 0; i < queues.Length && i < renderer.sharedMaterials.Length; i++)
+//			renderer.sharedMaterials[i].renderQueue = queues[i];
+//	}
+//	
+//}
