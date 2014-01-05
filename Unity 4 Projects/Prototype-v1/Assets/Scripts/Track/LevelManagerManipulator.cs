fd29191595bafc8387ee9efbf3ce8a7624601ae9
Clemens S <gs12m015@technikum-wien.at>
2014-01-05 16:27:56 +0100

    Configured 3 more levels.

---
+++ Unity 4 Projects/Prototype-v1/Assets/Scripts/Track/LevelManagerManipulator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManagerManipulator : MonoBehaviour 
+{
+	public int curLevel = 0;
+	// THIS IS FOR DEMO ONLY! DELETE THIS!
+	void Awake () {
+		LevelManager.CurrentLevel = curLevel;
+	}
+}
