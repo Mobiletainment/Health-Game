ae70bb3f5170238fef8ccaf3e360b1e9e80cd61f
Clemens S <gs12m015@technikum-wien.at>
2014-06-17 21:22:32 +0200

    Finished the AvatarState Scene Demo. (Not perfect, open for discussion, Letters are too small - hard to read.) Linked the AvatarState Scene with the Main Menu.

---
+++ Unity 4 Projects/Prototype-v1/Assets/Scripts/UI/DrawEnergyState.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawEnergyState : MonoBehaviour 
+{
+	public UISprite _spriteFull;
+	public UISprite _spriteLow;
+
+	public int size = 40; // square length
+	public int startposLeft = 120; // 120 px (to the right) from the left.
+	public int startposTop = -60; // -60 px (down) from the center.
+	public int distanceSide = 40;
+	public int distanceHeight = 20;
+
+	// Use this for initialization
+	private void Start() 
+	{
+		int currentEnergy = AvatarState.GetStateValue(AvatarState.State.CURRENT_ENERGY);
+
+		// The maxiumum energy value is 15. There will be drawn 3 rows with each 5 columns of energy symbols.
+		int counter = 0;
+		for(int row = 0; row < 3; ++row)
+		{
+			for(int col = 0; col < 5; ++col)
+			{
+				counter++;
+				UISprite curSprite = (counter <= currentEnergy ? _spriteFull : _spriteLow);
+				UISprite spriteInstance = Instantiate(curSprite, Vector3.zero, Quaternion.identity) as UISprite;
+				spriteInstance.transform.parent = transform;
+				spriteInstance.transform.localScale = Vector3.one;
+//				spriteInstance.SetRect(startposLeft + (col * (startposLeft + distanceSide)),
+//				                       startposTop + (row * (startposTop + distanceHeight)),
+//				                       size, size);
+				spriteInstance.topAnchor.target = transform;
+				spriteInstance.topAnchor.absolute = startposTop - (row * (distanceHeight + size)) + size;
+				spriteInstance.bottomAnchor.target = transform;
+				spriteInstance.bottomAnchor.absolute = startposTop - (row * (distanceHeight + size));
+				spriteInstance.leftAnchor.target = transform;
+				spriteInstance.leftAnchor.absolute = startposLeft + (col * (distanceSide + size));
+				spriteInstance.rightAnchor.target = transform;
+				spriteInstance.rightAnchor.absolute = startposLeft + (col * (distanceSide + size)) + size;
+			}
+
+			Debug.Log (startposTop - (row * (startposTop - distanceHeight)));
+		}
+
+//		UISprite spriteInstance = Instantiate(_spriteFull, Vector3.zero, Quaternion.identity) as UISprite;
+//		spriteInstance.transform.parent = transform;
+//		spriteInstance.transform.localScale = Vector3.one;
+	}
+}
---
+++ Unity 4 Projects/Prototype-v1/Assets/Scripts/UI/LoadScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadScene : MonoBehaviour 
+{
+	public string scene;
+
+	void OnClick()
+	{
+		if(scene == null)
+			Debug.LogError("Error: No Scene has been set!");
+
+		Application.LoadLevel(scene);
+	}
+}
